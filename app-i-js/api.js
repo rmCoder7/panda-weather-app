7f56b68ef372e5f4ecd9af25b353e99e2b9d33b2
rmCoder7 <mohammedayon2021@gmail.com>
2023-03-01 23:17:39 +0600

    first

---
+++ app-i-js/api.js
@@ -0,0 +1,40 @@
+const Api_key= `6aee9fc938162b96b97d710b586634b0`
+
+
+const loadTemperature= city=>{
+
+const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_key}&units=metric`
+ fetch(url)
+ .then(res=> res.json())
+ .then(data=>displayData(data)); 
+
+}
+
+
+const displayData = data=>{
+
+const displayTemperature = document.getElementById('temper');
+// console.log(data.main.temp)
+displayTemperature.innerText=data.main.temp
+
+}
+
+
+document.getElementById('btn-search').addEventListener('click', function(){
+
+const loadData = document.getElementById('search-filed');
+const city = loadData.value;
+// set city
+document.getElementById('city').innerText= city;
+loadTemperature(city);
+
+
+
+
+})
+
+
+
+
+loadTemperature('Dhaka')
+
